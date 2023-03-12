31d21fa083872d500c016b6b3b2587d25510702d
Ludovic Courtès <ludo@gnu.org>
2023-03-12 23:10:27 +0100

    Add REPL service.
    * modules/shepherd/service/repl.scm, tests/services/repl.sh: New files.
    * Makefile.am (dist_servicesub_DATA, TESTS): Add them.
    * doc/shepherd.texi (REPL Service): New section.
    * po/POTFILES.in: Add 'repl.scm'.

---
+++ modules/shepherd/service/repl.scm
@@ -0,0 +1,111 @@
+;; repl.scm -- Read-eval-print loop.
+;; Copyright (C) 2023 Ludovic Courtès <ludo@gnu.org>
+;;
+;; This file is part of the GNU Shepherd.
+;;
+;; The GNU Shepherd is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; The GNU Shepherd is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the GNU Shepherd.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (shepherd service repl)
+  #:use-module (shepherd service)
+  #:use-module (shepherd support)
+  #:use-module ((shepherd comm) #:select (open-server-socket))
+  #:use-module (fibers)
+  #:use-module (fibers channels)
+  #:use-module (fibers io-wakeup)
+  #:autoload   (system repl repl) (start-repl)
+  #:use-module (ice-9 match)
+  #:use-module (oop goops)
+  #:export (default-repl-socket-file
+            repl-service))
+
+(define (spawn-child-service client id)
+  "Register and start a new service that runs a REPL on @var{client}, a
+socket.  Use @var{id} to create the service name."
+  (letrec* ((name    (string->symbol
+                      (string-append "repl-client-"
+                                     (number->string id))))
+            (service (make <service>
+                       #:provides (list name)
+                       #:transient? #t
+                       #:start (lambda ()
+                                 (spawn-fiber
+                                  (lambda ()
+                                    (run-client-repl service client)))
+                                 client)
+                       #:stop (lambda (client)
+                                (close-port client)
+                                #f))))
+    (register-services service)
+    (start service)))
+
+(define* (run-repl-service socket)
+  (let loop ((client-id 1))
+    (match (accept socket (logior SOCK_NONBLOCK SOCK_CLOEXEC))
+      ((client . client-address)
+       ;; TRANSLATORS: "REPL" stands for "read-eval-print loop".
+       (local-output (l10n "Accepting REPL connection.")
+                     client-address)
+       (spawn-child-service client client-id)
+       (loop (+ client-id 1)))
+      (_ #f))))
+
+(define (spawn-repl-service socket)
+  "Spawn a REPL service that accepts connection on @var{socket}."
+  (spawn-fiber
+   (lambda ()
+     (run-repl-service socket)))
+  #t)
+
+(define user-module
+  (let ((module (resolve-module '(shepherd-user) #f #f #:ensure #t)))
+    (beautify-user-module! module)
+    (module-set! module 'sleep (@ (fibers) sleep)) ;avoid that pitfall
+    module))
+
+(define (run-client-repl service client)
+  "Return a REPL on @var{client}, a socket.  When the REPL terminates or
+crashes, stop @var{service}."
+  (catch #t
+    (lambda ()
+      (parameterize ((current-input-port client)
+                     (current-output-port client)
+                     (current-error-port client)
+                     (current-warning-port client))
+        (save-module-excursion
+         (lambda ()
+           (set-current-module user-module)
+           (with-fluids ((*repl-stack* '()))
+             (start-repl))))))
+    (lambda args
+      (local-output (l10n "Uncaught REPL exception: ~s.") args)))
+  (stop service))
+
+(define default-repl-socket-file
+  ;; Default socket file for the REPL.
+  (make-parameter (string-append default-socket-dir "/repl")))
+
+(define* (repl-service #:optional
+                       (socket-file (default-repl-socket-file)))
+  "Return a REPL service that listens to @var{socket-file}."
+  (make <service>
+    #:docstring (l10n "Run a read-eval-print loop (REPL).")
+    #:provides '(repl)
+    #:requires '()
+    #:start (lambda args
+              (let ((socket (open-server-socket socket-file)))
+                (spawn-repl-service socket)
+                socket))
+    #:stop (lambda (socket)
+             (close-port socket)
+             #f)))
---
+++ tests/services/repl.sh
@@ -0,0 +1,98 @@
+# GNU Shepherd --- Test monitoring service.
+# Copyright © 2023 Ludovic Courtès <ludo@gnu.org>
+#
+# This file is part of the GNU Shepherd.
+#
+# The GNU Shepherd is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or (at
+# your option) any later version.
+#
+# The GNU Shepherd is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the GNU Shepherd.  If not, see <http://www.gnu.org/licenses/>.
+
+shepherd --version
+herd --version
+
+socket="t-socket-$$"
+conf="t-conf-$$"
+log="t-log-$$"
+pid="t-pid-$$"
+repl_socket="$PWD/repl-socket-$$"
+
+herd="herd -s $socket"
+
+trap "cat $log || true;
+      rm -f $socket $repl_socket $conf $log;
+      test -f $pid && kill \`cat $pid\` || true; rm -f $pid" EXIT
+
+cat > "$conf" <<EOF
+(use-modules (shepherd service repl))
+
+(register-services (repl-service "$repl_socket"))
+EOF
+
+rm -f "$pid" "$log" "$repl_socket"
+shepherd -I -s "$socket" -c "$conf" -l "$log" --pid="$pid" &
+
+# Wait till it's ready.
+while ! test -f "$pid" ; do sleep 0.3 ; done
+
+$herd start repl
+$herd status repl | grep "started"
+
+$herd status
+test $($herd status | grep '^ ' | wc -l) -eq 2
+
+guile -c '(let ((sock (socket AF_UNIX SOCK_STREAM 0)))
+            (connect sock PF_UNIX "'$repl_socket'")
+            (sleep 10))' &
+child_pid=$!
+
+$herd status
+$herd status repl-client-1
+$herd status repl-client-1 | grep "started"
+$herd status repl-client-1 | grep "transient"
+test $($herd status | grep '^ ' | wc -l) = 3
+
+# Make sure 'repl-client-1' gets stopped as soon as the client disappears.
+kill $child_pid
+while test $($herd status | grep '^ ' | wc -l) -ne 2; do $herd status && sleep 1 ;done
+! $herd status repl-client-1
+
+guile -c '
+(use-modules (ice-9 rdelim))
+
+(setvbuf (current-output-port) (string->symbol "none"))
+(alarm 10)
+(let ((sock (socket AF_UNIX SOCK_STREAM 0)))
+  (connect sock PF_UNIX "'$repl_socket'")
+  (format #t "connected!~%> ")
+
+  (let loop ()
+    (define chr (read-char sock))
+    (unless (eof-object? chr)
+      (display chr)
+      (when (eq? chr #\newline)
+	(display "> ")))
+    (cond ((eof-object? chr)
+           (format #t "done!~%"))
+          ((eq? chr #\>)
+           (display "(+ 2 3)\n,q\n" sock)
+           (loop))
+	  (else
+	   (loop)))))
+'
+
+while test $($herd status | grep '^ ' | wc -l) -ne 2; do $herd status && sleep 1; done
+$herd stop repl
+$herd status repl | grep "stopped"
+
+# Now we can't connect anymore.
+! guile -c '(let ((sock (socket AF_UNIX SOCK_STREAM 0)))
+              (connect sock PF_UNIX "'$repl_socket'"))'
